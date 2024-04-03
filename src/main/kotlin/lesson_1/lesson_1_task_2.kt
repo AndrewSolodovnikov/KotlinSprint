f7541cdd35c297b4f696537f8bb45c07d828915b
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-03 21:29:16 +0300

    KS-1-2

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,9 @@
+package lesson_1
+
+fun main() {
+    var numberOfOrders: Int = 75
+    var purchaseMessage: String = "Спасибо за покупку"
+
+    println(numberOfOrders)
+    println(purchaseMessage)
+}

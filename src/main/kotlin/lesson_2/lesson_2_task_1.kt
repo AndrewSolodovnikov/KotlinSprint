e1ab1e57d0a2c3305418930e79fb3a7f4f3a1a28
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-05 23:44:37 +0300

    KS-2-1 fixed

--- src/main/kotlin/lesson_2/lesson_2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -1,5 +1,13 @@
 package lesson_2
 
 fun main() {
-    println((3+4+3+5) / 4f)
+    var grade1 = 3
+    var grade2 = 4
+    var grade3 = 3
+    var grade4 = 5
+    var numberOfStudents = 4
+
+    var averageGrade = ((grade1 + grade2 + grade3 + grade4).toFloat() / numberOfStudents)
+
+    println("%.2f".format(averageGrade))
 }

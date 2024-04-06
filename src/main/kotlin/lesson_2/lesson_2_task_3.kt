e2046f05641b24774318e9cf6c7edc905ca7ec9a
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-06 14:23:14 +0300

    KS-2-3 fixed

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -1,16 +1,15 @@
 package lesson_2
 
+const val ONE_MINUTE_IN_SECONDS = 60
 fun main() {
     val hour = 9
     val minute = 39
     val travelTime = 457
 
-    val departureTime = hour * 60 + minute
+    val departureTime = hour * ONE_MINUTE_IN_SECONDS + minute
 
-    val arrivalHour = (departureTime + travelTime) / 60
-    val arrivalMinute = (departureTime + travelTime) % 60
+    val arrivalHour = (departureTime + travelTime) / ONE_MINUTE_IN_SECONDS
+    val arrivalMinute = (departureTime + travelTime) % ONE_MINUTE_IN_SECONDS
 
-    print(arrivalHour)
-    print(":")
-    print(arrivalMinute)
+    print(String.format("%02d:%02d", arrivalHour, arrivalMinute))
 }

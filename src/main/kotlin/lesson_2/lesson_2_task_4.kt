dcdf4ca219ff1f97b0cc3c333b3bab378770e15c
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-06 14:35:22 +0300

    KS-2-4 fixed

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -1,10 +1,15 @@
 package lesson_2
 
+const val ONE_HUNDRED_PERCENT = 100
+
 fun main() {
     var crystalOre = 7
     var ironOre = 11
+    var gainPercentage = 20
 
-    var crystalBuff = ((1.2 * crystalOre) - crystalOre).toInt()
+    var crystalBuff = ((gainPercentage * crystalOre) / ONE_HUNDRED_PERCENT).toInt()
+    var ironBuff = ((gainPercentage * ironOre) / ONE_HUNDRED_PERCENT).toInt()
 
     println(crystalBuff)
+    println(ironBuff)
 }

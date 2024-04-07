30c17f9121f72bc0fe1f086667381293332e606b
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-07 15:21:33 +0300

    KS-3-2 fixed

--- src/main/kotlin/lesson_3/lesson_3_task_2.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -1,16 +1,17 @@
 package lesson_3
 
 fun main() {
-    val lastName = "Андреева"
-    val firstName = "Татьяна"
-    val middleName = "Сергеевна"
+    var lastName = "Андреева"
+    var firstName = "Татьяна"
+    var middleName = "Сергеевна"
     var age = 20
 
-    val marriedLastName = "Сидорова"
-    val marriedFirstName = "Татьяна"
-    val marriedMiddleName = "Сергеевна"
-    var marriedAge = 22
+    println("$lastName $firstName $middleName, $age")
+
+    lastName = "Сидорова"
+    firstName = "Татьяна"
+    middleName = "Сергеевна"
+    age = 22
 
     println("$lastName $firstName $middleName, $age")
-    println("$marriedLastName $marriedFirstName $marriedMiddleName, $marriedAge")
 }

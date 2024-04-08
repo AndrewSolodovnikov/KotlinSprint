4a1ce8f9d49eb0700eda4a2f3a7109e1e88a22e9
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-08 22:21:25 +0300

    KS-4-1 fixed

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -2,9 +2,8 @@ package lesson_4
 
 const val NUMBER_OF_TABLES = 13
 fun main() {
-    var tablesBookedToday = 13
-    var tablesBookedTomorrow = 9
-    var table: Boolean = true
+    val tablesBookedToday = 13
+    val tablesBookedTomorrow = 9
 
     println("Доступность столиков на сегодня: ${tablesBookedToday < NUMBER_OF_TABLES} \n" +
             "Доступность столиков на завтра: ${tablesBookedTomorrow < NUMBER_OF_TABLES}")

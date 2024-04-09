135eff4f2c63faac1c0c0a7422311690a4dec49b
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-09 22:19:38 +0300

    KS-4-3 fixed

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,17 +1,16 @@
 package lesson_4
 
+const val IS_SUNNY: Boolean = true
+const val IS_OUTDOOR_AWNING: Boolean = true
+const val AIR_HUMIDITY = 20
+const val SEASON = "зима"
 fun main() {
-    val sunny: Boolean = true
-    val outdoorAwning: Boolean = true
-    val airHumidity = 20
-    val season = "зима"
-
-    val sunnyNow: Boolean = true
-    val outdoorAwningNow: Boolean = true
+    val isSunnyNow: Boolean = true
+    val isOutdoorAwningNow: Boolean = true
     val airHumidityNow = 20
     val seasonNow = "зима"
 
     println("Благоприятные ли условия сейчас для роста бобовых? " +
-            "${sunny == sunnyNow && outdoorAwning == outdoorAwningNow && 
-                    airHumidity == airHumidityNow && season == seasonNow}")
+            "${IS_SUNNY == isSunnyNow && IS_OUTDOOR_AWNING == isOutdoorAwningNow && 
+                    AIR_HUMIDITY == airHumidityNow && SEASON !== seasonNow}")
 }

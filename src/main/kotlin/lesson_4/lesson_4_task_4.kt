0c665d685021be92f7a16893d6de2893d660498e
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-09 22:32:43 +0300

    KS-4-4 fixed

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -2,13 +2,12 @@ package lesson_4
 
 fun main(){
     val currentDay = 5
-    val trainingArmAndAbs = currentDay % 2 == 1
-    val trainingLegAndBack = currentDay % 2 == 0
+    val isEvenDay = currentDay % 2 == 0
 
     println("""
-        Упражнения для рук:    $trainingArmAndAbs
-        Упражнения для ног:    $trainingLegAndBack
-        Упражнения для спины:  $trainingLegAndBack
-        Упражнения для пресса: $trainingArmAndAbs
+        Упражнения для рук:    ${!isEvenDay}
+        Упражнения для ног:    $isEvenDay
+        Упражнения для спины:  $isEvenDay
+        Упражнения для пресса: ${!isEvenDay}
     """.trimIndent())
 }

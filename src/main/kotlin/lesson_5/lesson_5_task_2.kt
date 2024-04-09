728f77666ad76bd4edcf2df67ff758d89ec9a75d
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-09 23:21:33 +0300

    KS-5-2

---
+++ src/main/kotlin/lesson_5/lesson_5_task_2.kt
@@ -0,0 +1,19 @@
+package lesson_5
+
+const val AGE_OF_MAJORITY = 18
+const val CURRENT_YEAR = 2024
+fun main() {
+    println("Введите ваш год рождения")
+    val userBirthYear = readln()!!.toInt()
+    val userAge = CURRENT_YEAR - userBirthYear
+
+
+    if (userAge >= AGE_OF_MAJORITY) {
+        println("Показать экран со скрытым контентом")
+    } else if (userAge == 16 || userAge == 17) {
+        println("Показать экран с ограниченным контентом")
+    } else {
+        println("Верннуться на главный экран")
+    }
+
+}

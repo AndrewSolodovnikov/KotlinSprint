7a0978b2a81ed0250b07cd3b14419b86f406bcd7
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-11 23:51:51 +0300

    KS-5-5

---
+++ src/main/kotlin/lesson_5/lesson_5_task_5.kt
@@ -0,0 +1,30 @@
+package lesson_5
+
+import kotlin.random.Random
+
+const val HOW_MANY_NUMBERS = 3
+fun main() {
+    val userNumbers = mutableListOf<Int>()
+    val randomNumbers = List(HOW_MANY_NUMBERS) { Random.nextInt(0, 42) }
+
+    repeat(HOW_MANY_NUMBERS) {
+        println("Введите число от 0 до 42:")
+        val number = readlnOrNull()?.toInt()
+        if (number != null && number in 0..42) {
+            userNumbers.add(number)
+        } else {
+            println("Введено неверное значение")
+        }
+    }
+
+    val numberOfMatches = randomNumbers.intersect(userNumbers.toSet()).size
+
+    when (numberOfMatches) {
+        3 -> println("Вы угадали все числа и выйграли джекпот!")
+        2 -> println("Вы угадали 2 числа и выйграли крупную сумму!")
+        1 -> println("Вы угадали 1 число и получаете утешительный приз!")
+        0 -> println("Вы не угадали ни одного числа, в следующий раз удача вам улыбнется;)")
+    }
+
+    println("Выйгрышные числа: ${randomNumbers.joinToString( )}")
+}

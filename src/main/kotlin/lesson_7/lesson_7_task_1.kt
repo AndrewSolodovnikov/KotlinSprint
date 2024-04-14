ae525ad4ad2b180ccda8b8317c89222f8420956d
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-14 14:06:53 +0300

    KS-7-1 fixed

--- src/main/kotlin/lesson_7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -4,12 +4,15 @@ const val CHARACTERS_IN_PASSWORD = 6
 
 fun main() {
     var generatedPassword = ""
+    val rangeNumbers = 1..9
+    val rangeLetters = 'a'..'z'
+
     for (character in 1..CHARACTERS_IN_PASSWORD) {
         if (character % 2 == 0) {
-            val number = (1..9).random()
+            val number = (rangeNumbers).random()
             generatedPassword = generatedPassword.plus(number)
         } else {
-            val letter = ('a'..'z').random()
+            val letter = (rangeLetters).random()
             generatedPassword = generatedPassword.plus(letter)
         }
     }

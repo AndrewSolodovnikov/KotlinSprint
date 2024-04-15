2af70e073565e3cff0471a361ed92061de41761e
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-15 22:06:58 +0300

    KS-7-3 fixed

--- src/main/kotlin/lesson_7/lesson_7_task_3.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_3.kt
@@ -4,9 +4,7 @@ fun main() {
     println("Введите число:")
     val enteredNumber = readln().toInt()
 
-    for (number in 0 .. enteredNumber) {
-        if (number % 2 == 0) {
+    for (number in 0 .. enteredNumber step 2) {
             println(number)
-        }
     }
 }

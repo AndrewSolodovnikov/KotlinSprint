27271afd9bbabcbf84dfa61d80666fee1cb676b9
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-15 22:13:45 +0300

    KS-7-4 fixed

--- src/main/kotlin/lesson_7/lesson_7_task_4.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_4.kt
@@ -4,9 +4,9 @@ fun main() {
     println("Введите количество секунд:")
     var enteredSeconds = readln().toInt()
 
-    for (second in 1 .. enteredSeconds) {
+    for (second in enteredSeconds downTo 1) {
         Thread.sleep(1000)
-        println("Осталось секунд до конца ${--enteredSeconds}")
+        println("Осталось секунд до конца ${second - 1}")
     }
 
     println("Время вышло")

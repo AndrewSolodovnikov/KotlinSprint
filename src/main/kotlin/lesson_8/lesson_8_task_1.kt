656da236d09c6e6a964dcd206b5f9b33cd75edfd
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-16 22:20:25 +0300

    KS-8-1 fixed

--- src/main/kotlin/lesson_8/lesson_8_task_1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -3,17 +3,14 @@ package lesson_8
 const val DAY_OF_STATISTICS = 7
 fun main() {
     val rangeAdImpressions = (40000..50000)
-    var arrayAdImpressions: ArrayList<Int> = arrayListOf()
+    var arrayAdImpressions: Array<Int> = arrayOf()
 
     for (day in 1..DAY_OF_STATISTICS) {
         val adImpressions = rangeAdImpressions.random()
-        arrayAdImpressions.add(adImpressions)
+        arrayAdImpressions += adImpressions
     }
 
-    var sum = 0
-    for (day in arrayAdImpressions) {
-        sum += day
-    }
+    val weekAdImpressions = arrayAdImpressions.sum()
 
-    println(sum)
+    println(weekAdImpressions)
 }

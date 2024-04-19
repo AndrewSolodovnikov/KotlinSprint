bf9083fb458335515fa286f268036c5f7ee4ee7c
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-19 22:41:20 +0300

    KS-9-3 fixed

--- src/main/kotlin/lesson_9/lesson_9_task_3.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_3.kt
@@ -1,11 +1,13 @@
 package lesson_9
 
 fun main() {
-    val listIngredients = listOf<Int>(2,50,15)
+    val listIngredients = listOf<Int>(2, 50, 15)
     println("Введите количество порций:")
     val numberOfServings = readln().toInt()
 
     val newListIngredients = listIngredients.map { numberOfServings * it }
-    println("На $numberOfServings порций вам понадобится: Яиц – ${newListIngredients[0]}, " +
-            "молока – ${newListIngredients[1]}, сливочного масла – ${newListIngredients[2]}")
+    println(
+        "На $numberOfServings порций вам понадобится: Яиц – ${newListIngredients[0]}, " +
+                "молока – ${newListIngredients[1]}, сливочного масла – ${newListIngredients[2]}"
+    )
 }

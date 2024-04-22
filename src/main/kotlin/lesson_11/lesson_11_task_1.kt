84e17db82dbd25f7fc89cd87dff397d911a9f27a
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-22 23:21:08 +0300

    KS-11-1

--- src/main/kotlin/lesson_11/lesson_11_task_1.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_1.kt
@@ -4,13 +4,13 @@ fun main() {
     val user1 = User(1111, "admin", "qwerty", "aaa@mail.ru")
     val user2 = User(2222, "user", "rdxzse", "zzz@mail.ru")
 
-    println("Данные первого пользователя:")
+    println("Данные первого пользователя: ")
     println(user1.id)
     println(user1.login)
     println(user1.password)
     println(user1.email)
 
-    println("Данные второго пользователя:")
+    println("Данные второго пользователя: ")
     println(user2.id)
     println(user2.login)
     println(user2.password)

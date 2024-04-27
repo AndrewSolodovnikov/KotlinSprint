2348aadf993bcf05dcf970aaf670426b4914164c
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-27 23:23:22 +0300

    KS-11-4 fixed

--- src/main/kotlin/lesson_11/lesson_11_task_4.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_4.kt
@@ -2,29 +2,27 @@ package lesson_11
 
 fun main() {
     val category = Category("Бургеры", "image.jpg", mutableListOf())
-    val reciep = Reciep("Жирный бургер", "image1.jpg", false, 1, mutableListOf())
+    val recipe = Recipe("Жирный бургер", "image1.jpg", false, 1, mutableListOf())
 
-    category.addSubcategory(Subcategory("Сочные бургеры", "image2.jpg", "Самые сочные бургеры"))
-    category.addSubcategory(Subcategory("Бургеры с курицей", "image2.jpg", "Бургеры с оболденной курочкой"))
+    category.addRecipe(Recipe("Сочные бургеры", "image2.jpg", false, 1, mutableListOf()))
+    category.addRecipe(Recipe("Бургеры с курицей", "image2.jpg", false, 1, mutableListOf()))
 
-    reciep.addReciep(Ingredients("Булочка", 1, "шт."))
-    reciep.addReciep(Ingredients("Котлета", 1, "шт."))
-    reciep.addReciep(Ingredients("Сыр", 100, "гр."))
+    recipe.addIngredient(Ingredient("Булочка", 1, "шт."))
+    recipe.addIngredient(Ingredient("Котлета", 1, "шт."))
+    recipe.addIngredient(Ingredient("Сыр", 100, "гр."))
 }
 
-class Category(val title: String, val image: String, val subcategoryList: MutableList<Subcategory>){
-    fun addSubcategory(subcategory: Subcategory) {
-        subcategoryList.add(subcategory)
+class Category(val title: String, val image: String, val recipeList: MutableList<Recipe>){
+    fun addRecipe(recipe: Recipe) {
+        recipeList.add(recipe)
     }
 }
 
-class Reciep(val title: String, val image: String, var like: Boolean, var numberOfIngredients: Int,
-             val ingredientsList: MutableList<Ingredients>) {
-    fun addReciep(ingredients: Ingredients) {
-        ingredientsList.add(ingredients)
+class Recipe(val title: String, val image: String, var like: Boolean, var numberOfIngredients: Int,
+             val ingredientsList: MutableList<Ingredient>) {
+    fun addIngredient(ingredient: Ingredient) {
+        ingredientsList.add(ingredient)
     }
 }
 
-class Subcategory(val title: String, val image: String, val description: String)
-
-class Ingredients(val ingredient: String, val number: Int, val unit: String)
+class Ingredient(val ingredient: String, val number: Int, val unit: String)

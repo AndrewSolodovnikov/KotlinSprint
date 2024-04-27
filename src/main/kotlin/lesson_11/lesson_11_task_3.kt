d6185e3ff34fb0ddce77a7e0f6170f751c1aeda5
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-27 22:10:56 +0300

    KS-11-3 fixed

--- src/main/kotlin/lesson_11/lesson_11_task_3.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -2,9 +2,9 @@ package lesson_11
 
 fun main() {
     val room = ChatRoom("cover.jpg", "Флудилка")
-    room.addParticipants(Participants("user1", "avatar1.jpg", "разговаривает"))
-    room.addParticipants(Participants("user2", "avatar2.jpg", "микрофон выключен"))
-    room.addParticipants(Participants("user3", "avatar3.jpg", "пользователь заглушен"))
+    room.addParticipants(Participant("user1", "avatar1.jpg", "разговаривает"))
+    room.addParticipants(Participant("user2", "avatar2.jpg", "микрофон выключен"))
+    room.addParticipants(Participant("user3", "avatar3.jpg", "пользователь заглушен"))
 
     room.statusUpdate("user1", "микрофон выключен")
     room.printUserInfo("user1")
@@ -13,8 +13,8 @@ fun main() {
 
 class ChatRoom(val cover: String,
                val title: String,
-               val participantsList: MutableList<Participants> = mutableListOf()) {
-    fun addParticipants(user: Participants) {
+               val participantsList: MutableList<Participant> = mutableListOf()) {
+    fun addParticipants(user: Participant) {
         participantsList.add(user)
     }
 
@@ -31,4 +31,4 @@ class ChatRoom(val cover: String,
 
 }
 
-class Participants(val login: String, val avatar: String, var status: String)
+class Participant(val login: String, val avatar: String, var status: String)

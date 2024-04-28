6745932bcf292da4f850f89b59f73a811651b71c
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-28 23:42:12 +0300

    KS-12-4

---
+++ src/main/kotlin/lesson_12/lesson_12_task_4.kt
@@ -0,0 +1,21 @@
+package lesson_12
+
+fun main() {
+    println("День 1:")
+    val weather1 = WeatherInfo(300,290,false)
+    println("День 2:")
+    val weather2 = WeatherInfo(295, 280, true)
+}
+
+class WeatherInfo(dayTemperatureKelvin: Int, nightTemperatureKelvin: Int, precipitation: Boolean) {
+    val kelvinTemperature = 273.15
+    var dayTemperatureCelsius: Int = (dayTemperatureKelvin - kelvinTemperature).toInt()
+    var nightTemperatureCelsius: Int = (nightTemperatureKelvin - kelvinTemperature).toInt()
+    var precipitation: Boolean = precipitation
+
+    init {
+        println("Дневная температура: $dayTemperatureCelsius")
+        println("Дневная температура: $nightTemperatureCelsius")
+        println("Сегодня ожидается дождь: ${if (precipitation) "да" else "нет"}")
+    }
+}

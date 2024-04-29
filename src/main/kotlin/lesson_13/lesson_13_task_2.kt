79123aa6634187d5f2e32880ca7197314fcc4070
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-29 14:35:20 +0300

    KS-13-2

--- src/main/kotlin/lesson_13/lesson_13_task_2.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_2.kt
@@ -2,12 +2,13 @@ package lesson_13
 
 fun main() {
     val phoneDirectory = PhoneDirectory("Антон", 89001001010, null)
-
 }
 
 class PhoneDirectory(name: String, number: Long, company: String?) {
     init {
-        println("Имя: $name \nТелефон: $number \n" +
-                "${if (company != null) "Компания: $company" else "<не указано>"}")
+        println(
+            "Имя: $name \nТелефон: $number \n" +
+                    "${if (company != null) "Компания: $company" else "<не указано>"}"
+        )
     }
 }

ea217d6037720f66a495cd0e5fcba7e56d2ef8f5
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-04-30 14:58:15 +0300

    KS-13-3 fixed

--- src/main/kotlin/lesson_13/lesson_13_task_3.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_3.kt
@@ -6,15 +6,12 @@ fun main() {
         PhoneInfo("Вася", 89081001010, null),
         PhoneInfo("Георгий", 89995551010, "null"),
         PhoneInfo("Антонина", 89079001010, "Google"),
-        PhoneInfo("Виталий", 89067891010, "Apple"))
+        PhoneInfo("Виталий", 89067891010, "Apple")
+    )
 
-    contacts.forEach {
+    contacts.filter { it.company != null && it.company != "null" }.forEach {
         println("Имя: ${it.name} \nТелефон: ${it.number} \nКомпания: ${it.company}")
     }
 }
 
-class PhoneInfo(name: String, number: Long, company: String?) {
-    val name: String = name
-    val number: Long = number
-    val company: String? = company
-}
+class PhoneInfo(val name: String, val number: Long, val company: String?)

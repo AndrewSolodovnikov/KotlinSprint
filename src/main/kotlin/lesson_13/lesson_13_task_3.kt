104c3431c45208f4ec12a4ca8d14bdbc71bd03c6
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-05-03 07:03:41 +0300

    KS-13-3 fixed

--- src/main/kotlin/lesson_13/lesson_13_task_3.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_3.kt
@@ -3,7 +3,7 @@ package lesson_13
 fun main() {
     val contacts = listOf(
         PhoneInfo("Антон", 89001001010, null),
-        PhoneInfo("Вася", 89081001010, null),
+        PhoneInfo("Василий", 89081001010, null),
         PhoneInfo("Георгий", 89995551010, "null"),
         PhoneInfo("Антонина", 89079001010, "Google"),
         PhoneInfo("Виталий", 89067891010, "Apple")

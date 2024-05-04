a38f684651a3300848a59f451a12a45f0a9cafc8
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-05-04 23:38:24 +0300

    KS-14-3

--- src/main/kotlin/lesson_14/lesson_14_task_3.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_3.kt
@@ -3,26 +3,57 @@ package lesson_14
 import kotlin.math.PI
 
 fun main() {
-    val listFigure = listOf(
-        Circle("white", 2.7),
-        Circle("white", 5.5),
-        Circle("white", 9.2),
-        Circle("black", 4.4),
-        Circle("black", 12.2),
+    val white = "white"
+    val black = "black"
 
-        Rectangle("white", 3.3, 2.1),
-        Rectangle("white", 4.2, 3.2),
-        Rectangle("black", 7.3, 5.1),
-        Rectangle("black", 8.3, 4.7),
-        Rectangle("black", 5.8, 4.6)
+    val listFigure = listOf(
+        Circle(white, 2.7),
+        Circle(white, 5.5),
+        Circle(white, 9.2),
+        Circle(black, 4.4),
+        Circle(black, 12.2),
+
+        Rectangle(white, 3.3, 2.1),
+        Rectangle(white, 4.2, 3.2),
+        Rectangle(black, 7.3, 5.1),
+        Rectangle(black, 8.3, 4.7),
+        Rectangle(black, 5.8, 4.6)
     )
 
-    val sumPerimetersBlackFigure = listFigure.filter { it.color == "black" }.sumOf { it.calculatePerimeter() }
-    val sumPerimetersWhiteFigure = listFigure.filter { it.color == "white" }.sumOf { it.calculatePerimeter() }
+    // вариант 1
+    var sumPerimetersBlackFigureForEach : Double = 0.0
+    var sumPerimetersWhiteFigureForEach : Double = 0.0
+    var sumAreaBlackFigureForEach : Double = 0.0
+    var sumAreaWhiteFigureForEach : Double = 0.0
 
-    val sumAreaBlackFigure = listFigure.filter { it.color == "black" }.sumOf { it.calculateArea() }
-    val sumAreaWhiteFigure = listFigure.filter { it.color == "white" }.sumOf { it.calculateArea() }
+    listFigure.forEach {
+        if (it.color == white) {
+            sumAreaWhiteFigureForEach += it.calculateArea()
+            sumPerimetersWhiteFigureForEach += it.calculatePerimeter()
+        } else {
+            sumAreaBlackFigureForEach += it.calculateArea()
+            sumPerimetersBlackFigureForEach += it.calculatePerimeter()
+        }
+    }
 
+    println("Вариант 1")
+    println(sumPerimetersBlackFigureForEach)
+    println(sumPerimetersWhiteFigureForEach)
+    println(sumAreaBlackFigureForEach)
+    println(sumAreaWhiteFigureForEach)
+
+    // вариант 2
+    val listBlackFigure = listFigure.filter { it.color == "black" }
+    val listWhiteFigure = listFigure.filter { it.color == "white" }
+
+    val sumPerimetersBlackFigure = listBlackFigure.sumOf { it.calculatePerimeter() }
+    val sumPerimetersWhiteFigure = listWhiteFigure.sumOf { it.calculatePerimeter() }
+
+    val sumAreaBlackFigure = listBlackFigure.sumOf { it.calculateArea() }
+    val sumAreaWhiteFigure = listWhiteFigure.sumOf { it.calculateArea() }
+
+    println()
+    println("Вариант 2")
     println(sumPerimetersBlackFigure)
     println(sumPerimetersWhiteFigure)
     println(sumAreaBlackFigure)

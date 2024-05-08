d3ccb446bd43c10dfd51bfba5f2781e1f405beaa
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-05-08 22:38:12 +0300

    KS-15-4 fixed

--- src/main/kotlin/lesson_15/lesson_15_task_4.kt
+++ src/main/kotlin/lesson_15/lesson_15_task_4.kt
@@ -4,23 +4,19 @@ fun main() {
     val instrument = Instrument("Скрипка")
     val accessories = Accessories("Струны")
 
-    instrument.search()
-    accessories.search()
+    instrument.search(accessories)
 }
 
 interface SearchInstrument {
-    fun search()
+    fun search(accessories: Accessories)
 }
 
-class Instrument (val name: String) : SearchInstrument {
-    override fun search() {
-        println("Выполняется поиск инструмента: $name")
+open class Product (val name: String)
+
+class Instrument (name: String) : Product(name), SearchInstrument {
+    override fun search(accessories: Accessories) {
+        println("Выполняется поиск аксесуара \"${accessories.name}\" для инструмента: $name")
     }
 }
 
-class Accessories (val name: String) : SearchInstrument {
-    override fun search() {
-        println("Выполняется поиск аксессуара: $name")
-    }
-
-}
+class Accessories (name: String) : Product(name)

0027ae5d112cc667bd45eb2bfbe3b0b9fb09152c
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-05-22 22:07:10 +0300

    KS-16-3 fixed

--- src/main/kotlin/lesson_16/lesson_16_task_3.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_3.kt
@@ -1,16 +1,14 @@
 package lesson_16
 
 fun main() {
-    val customer = Customer()
+    val customer = Customer("aaa", "dggfd333")
 
     println("Введите пароль:")
     val enteredPassword = readln()
     customer.validatePassword(enteredPassword)
 }
 
-class Customer {
-    private val login: String = "aaa"
-    private val password = "jyuy213"
+class Customer(val login: String, private val password: String) {
 
     fun validatePassword(enteredPassword: String) {
         if (enteredPassword == password) println("Вы ввели верный пароль") else println("Введен неверный пароль")

3c48c324a4ef052c8fd6996a0972896053051636
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-05-28 22:35:34 +0300

    KS-20-1 fixed

--- src/main/kotlin/lesson_20/lesson_20_task_1.kt
+++ src/main/kotlin/lesson_20/lesson_20_task_1.kt
@@ -3,6 +3,6 @@ package lesson_20
 fun main() {
     val printString: (String) -> Unit = { username: String -> println("С наступающим Новым Годом, $username!") }
 
-    val username = readlnOrNull()?.takeIf { it.isNotBlank() } ?: "Аноним"
+    val username = readln().takeIf { it.isNotBlank() } ?: "Аноним"
     printString(username)
 }

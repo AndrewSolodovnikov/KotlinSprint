e3ebf072955ece472f54b698c59459c1b1c75d99
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-05-29 22:35:21 +0300

    KS-20-3

---
+++ src/main/kotlin/lesson_20/lesson_20_task_3.kt
@@ -0,0 +1,15 @@
+package lesson_20
+
+fun main() {
+    val checkKey: (MyPlayer) -> Unit = { myPlayer: MyPlayer ->
+        if (myPlayer.isKey) println("Дверь открыта") else println("Дверь закрыта")
+    }
+
+    val myPlayer1 = MyPlayer("Антон", true)
+    val myPlayer2 = MyPlayer("Георгий", false)
+
+    checkKey(myPlayer1)
+    checkKey(myPlayer2)
+}
+
+class MyPlayer(val name: String, var isKey: Boolean)

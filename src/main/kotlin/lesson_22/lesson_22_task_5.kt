444edc7efa34f060abe68899eac617de40562a33
AndrewSolodovnikov <151715887+AndrewSolodovnikov@users.noreply.github.com>
2024-06-07 22:54:38 +0300

    KS-22-5

---
+++ src/main/kotlin/lesson_22/lesson_22_task_5.kt
@@ -0,0 +1,22 @@
+package lesson_22
+
+fun main() {
+    val alphaCentauri = GalacticGuide(
+        "Альфа Центавр",
+        "тройная звёздная система в созвездии Центавра",
+        "07-06-2024",
+        4.36)
+
+    val (namePlaceOrEven, descriptionPlaceOrEven, dateOrTime, distanceFromEarth) = alphaCentauri
+
+    println("$namePlaceOrEven - $descriptionPlaceOrEven \n" +
+            "Дата фиксации события: $dateOrTime \n" +
+            "Расстояние от Земли: $distanceFromEarth св. лет")
+}
+
+data class GalacticGuide(
+    val namePlaceOrEven: String,
+    val descriptionPlaceOrEven: String,
+    val dateOrTime: String,
+    val distanceFromEarth: Double,
+    )
